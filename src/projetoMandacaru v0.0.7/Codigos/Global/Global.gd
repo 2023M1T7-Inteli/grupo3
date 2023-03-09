a54084f8923e20f957a833a522a79ac59bfa3796
Pelinsari <gabriel.ribeiro@sou.inteli.edu.br>
2023-03-09 10:22:14 -0300

    projetoMandacaru v0.0.7

---
+++ src/projetoMandacaru v0.0.7/Codigos/Global/Global.gd
@@ -0,0 +1,11 @@
+extends Node
+#página de variáveis globais
+
+#variável que retorna uma booleana para o início ou não do jogo
+var iniciou = false
+#variável que cria uma array vazia para implementação dos movimentos do personagem
+var lista = []
+#variável que acumula um valor para o contador que localizará valores na array "lista"
+var contador = 0
+#define a variável zerar para a lista como false
+var zerar =  false
