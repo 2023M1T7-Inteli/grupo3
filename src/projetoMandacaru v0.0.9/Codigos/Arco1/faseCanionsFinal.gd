5f046cdf3537c484a9b3052ce28a6caa0e10f94f
Pelinsari <gabriel.ribeiro@sou.inteli.edu.br>
2023-03-10 10:20:49 -0300

    projetoMandacaru comentários

--- src/projetoMandacaru v0.0.9/Codigos/Arco1/faseCanionsFinal.gd
+++ src/projetoMandacaru v0.0.9/Codigos/Arco1/faseCanionsFinal.gd
@@ -6,7 +6,8 @@ func _ready():
 	$personagemCena/corpoJogador/animacaoPersonagem.play("Direita")
 	#aciona a cutscene final
 	$cutCenaFinalCanions.play("cutFaseCanionsFinal")
-
-
+	
+#função que inicia quando um corpo entra na area "areaCabana"
 func _on_areaCabana_body_entered(body):
+	#muda a cena para a cena do pescador
 	get_tree().change_scene("res://Telas/Arco1/cenaPescador.tscn")
