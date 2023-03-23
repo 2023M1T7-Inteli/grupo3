10ecd57b6883432dced0d7071d4ce43b351d2cc1
DiogoBurgierman <124000765+DiogoBurgierman@users.noreply.github.com>
2023-03-23 15:30:13 -0300

    animacao da cena do festival adicionada

---
+++ src/projetoMandacaru v0.1.9/Codigos/Arco1/faseCanions2.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _on_Area2D_body_entered(body):
+	#cria uma árvore de cenas que muda a cena para a fase final
+	get_tree().change_scene("res://Telas/Arco1/faseCanions3.tscn")
+	#reinicia a lista global
+	Global.lista = []
+
+
+func _on_AreaItem_body_entered(body):
+	if body == $personagemCena/corpoJogador:
+		$AreaItem.visible = false
+		Global.itens['pipoca'][0] = true
+		Global.get_node("item").play()
